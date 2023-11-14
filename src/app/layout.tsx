422108c6eee830e46363e89e4595a8fd1a67cc50
clay21c <clay21c99@gmail.com>
2023-11-14 15:43:53 -0500

    fisrt app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -16,7 +17,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li><Link href="/about">About</Link></li>
+        </ul>
+      </nav>
+        {children}
+      </body>
     </html>
   )
 }
